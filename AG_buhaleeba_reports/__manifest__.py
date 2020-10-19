064e2fa45c1e2653b3d22aa57625f0bd4c9d91c6
Fouad Bitar <fouadb@lict.sy>
2020-10-19 11:19:04 +0200

    add custom report

---
+++ AG_buhaleeba_reports/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'AG_buhaleeba Reports',
+    'author': 'Fouad',
+    'depends': ['sale'],
+    'data': [
+        
+        'report/delivery_slip_inheirt.xml',
+        'report/cridet_note_inheirt.xml',
+        'report/purchase_quotation.xml',
+        'report/purchase_order_inheirt.xml',
+       
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
