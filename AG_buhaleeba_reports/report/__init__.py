3c77258648558c597b92522e41c63d35478dbe5d
Fouad Bitar <fouadb@lict.sy>
2020-10-22 14:27:18 +0200

    edite manifest file 3

--- AG_buhaleeba_reports/report/__init__.py
+++ AG_buhaleeba_reports/report/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# from . import prod_schedule_report
-# from . import work_order_report
+from . import delivery_slip_inheirt
+from . import invoice_inheirt
+from . import order_purchase_inheirt
+from . import quotation_purchase_inheirt
