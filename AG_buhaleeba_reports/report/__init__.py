9df1ffa8b21da874aacaa9c0f40e85c2e5b0794e
bincy296 <71172217+bincy296@users.noreply.github.com>
2020-10-24 07:53:53 +0000

    Update __init__.py
    changes done to remove import error

--- AG_buhaleeba_reports/report/__init__.py
+++ AG_buhaleeba_reports/report/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import delivery_slip_inheirt
-from . import invoice_inheirt
-from . import order_purchase_inheirt
-from . import quotation_purchase_inheirt
+#from . import delivery_slip_inheirt
+#from . import invoice_inheirt
+#from . import order_purchase_inheirt
+#from . import quotation_purchase_inheirt
