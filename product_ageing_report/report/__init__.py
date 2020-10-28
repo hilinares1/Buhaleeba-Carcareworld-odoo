ccd78fdfbd2fb84ccda5375b21376de1aac870b6
bincy296 <71172217+bincy296@users.noreply.github.com>
2020-10-28 10:51:10 +0000

    Add files via upload
    product_ageing(report)

---
+++ product_ageing_report/report/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import report_ageing_products
+# from . import xlsx_report_ageing_products
+
---
+++ product_ageing_report/wizard/product_ageing.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class AgeingAnalysis(models.TransientModel):
+    _name = 'product.ageing'
+
+    from_date = fields.Datetime(string="Starting Date", required=True)
+    location_id = fields.Many2many('stock.location', string="Location", domain=[('usage', '=', 'internal')])
+    product_categ = fields.Many2many('product.category', string="Category")
+    interval = fields.Integer(string="Interval(days)", default=30, required=True)
+    filter_by = fields.Selection([
+        ('location', "Location"), ('lot', "Lot"),
+    ], string="Expand By", default='lot')
+
+    @api.model
+    def compute_ageing(self, data):
+        """Redirects to the report with the values obtained from the wizard
+                'data['form']':  date duration"""
+        rec = self.browse(data)
+        data = {}
+        data['form'] = rec.read(['from_date', 'location_id', 'product_categ', 'interval'])
+        return self.env.ref('product_ageing_report.report_product_ageing').report_action(self,data=data)
+
+    @api.model
+    def xlsx_ageing_report(self, data):
+        rec = self.browse(data)
+        data = {}
+        data['form'] = rec.read(['from_date', 'location_id', 'product_categ', 'interval', 'filter_by'])
+        if not rec.filter_by:
+            raise ValidationError('Please select a Expand Option for Report')
+        return self.env.ref('product_ageing_report.report_product_ageing_xlsx').report_action(self, data=data)
