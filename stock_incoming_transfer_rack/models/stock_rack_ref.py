fdc98d50405b4588761a8b3304c5daa98f8c3e61
bincy296 <71172217+bincy296@users.noreply.github.com>
2020-11-14 10:19:27 +0000

    Update stock_rack_ref.py
    modifed(sana)

--- stock_incoming_transfer_rack/models/stock_rack_ref.py
+++ stock_incoming_transfer_rack/models/stock_rack_ref.py
@@ -5,19 +5,33 @@ from odoo import models, fields, api
 
 class StockRackRef(models.Model):
     _name = 'stock.rack.shelf'
+    _rec_name = 'complete_name'
 
     name = fields.Char(
         string='Name',
-        require=True,
+        required=True,
     )
     code = fields.Char(
         string='Code',
-        require=True,
+        required=True,
     )
     parent_id = fields.Many2one(
         'stock.rack.shelf',
         index=True,
         ondelete='cascade',
     )
+    complete_name = fields.Char(
+        'Complete Name',
+        compute='_compute_complete_name',
+        store=True
+    )
+    
+    @api.depends('name', 'parent_id.complete_name')
+    def _compute_complete_name(self):
+        for rack_shelf in self:
+            if rack_shelf.parent_id:
+                rack_shelf.complete_name = '%s / %s' % (rack_shelf.name , rack_shelf.parent_id.complete_name)
+            else:
+                rack_shelf.complete_name = rack_shelf.name
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
