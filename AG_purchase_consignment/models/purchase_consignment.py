614a1e6e545d94b7647d95a18192b239188afb7d
bincy296 <71172217+bincy296@users.noreply.github.com>
2020-11-21 13:18:30 +0000

    Add files via upload
    purchase_consignment

---
+++ AG_purchase_consignment/models/purchase_consignment.py
@@ -0,0 +1,18 @@
+from odoo import api, fields, models, SUPERUSER_ID, _
+import odoo.addons.decimal_precision as dp
+from odoo.exceptions import except_orm, ValidationError ,UserError
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    is_purchase_consignment = fields.Boolean(string='Is Purchase Consignment')
+
+    def button_confirm(self): #change in demo
+        for record in self:
+            res = super(PurchaseOrder, self).button_confirm()
+
+            if record.picking_ids:
+                if record.is_purchase_consignment == 'True':
+                    for pick in record.picking_ids:
+                        pick.move_lines.write({'origin': record.interchanging_po_sequence})
+                        pick.move_lines.write({'owner_id': record.partner_id})
