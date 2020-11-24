6e0c1888a7281354d34c15a178ff689965237c89
bincy296 <71172217+bincy296@users.noreply.github.com>
2020-11-24 03:33:55 +0000

    Update purchase_consignment.py
    modified purchase-consignment(bincy)

--- AG_purchase_consignment/models/purchase_consignment.py
+++ AG_purchase_consignment/models/purchase_consignment.py
@@ -2,19 +2,23 @@ from odoo import api, fields, models, SUPERUSER_ID, _
 import odoo.addons.decimal_precision as dp
 from odoo.exceptions import except_orm, ValidationError ,UserError
 
-class PurchaseOrder(models.Model):
+
+
+
+
+class PurchaseOrders(models.Model):
     _inherit = "purchase.order"
 
-    is_purchase_consignment = fields.Boolean(string='Is Consignment Purchase')
+    is_purchase_consignment = fields.Boolean(string='Is Purchase Consignment')
 
-    def button_confirm(self): #change in demo
-        for record in self:
-            res = super(PurchaseOrder, self).button_confirm()
-            if record.is_purchase_consignment == True:
-                
-                record.picking_ids.write({'owner_id': record.partner_id.id})
+    def button_approve(self):
+
+        res = super(PurchaseOrders, self).button_approve()
+        for order in self:
+            if order.is_purchase_consignment == True:
+
+                order.picking_ids.write({'owner_id': order.partner_id.id})
             else:
-                record.picking_ids.write({'owner_id': False})
-                
+                order.picking_ids.write({'owner_id': False})
 
-            return res
+        return res
