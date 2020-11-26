5dc67cf387344b914701dbc6780414c056ccd44f
bincy296 <71172217+bincy296@users.noreply.github.com>
2020-11-26 06:09:59 +0000

    Update account_invoice.py

--- om_account_asset/models/account_invoice.py
+++ om_account_asset/models/account_invoice.py
@@ -4,7 +4,6 @@
 from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
-import numpy
 
 
 class AccountInvoice(models.Model):
@@ -43,8 +42,6 @@ class AccountInvoiceLine(models.Model):
     asset_end_date = fields.Date(string='Asset End Date', compute='_get_asset_date', readonly=True, store=True)
     asset_mrr = fields.Float(string='Monthly Recurring Revenue', compute='_get_asset_date', readonly=True,
                              digits="Account", store=True)
-    #sale_value=fields.Float(store=True)
-    #asset_acc_id = fields.Many2one('account.asset.asset')
 
     @api.depends('asset_category_id', 'move_id.invoice_date')
     def _get_asset_date(self):
@@ -67,27 +64,23 @@ class AccountInvoiceLine(models.Model):
                     rec.asset_end_date = end_date
 
     def asset_create(self):
-
         if self.asset_category_id:
-            for x in numpy.arange(self.quantity):
-                vals = {
-                    'name': self.name,
-                    'code': self.name or False,
-                    'category_id': self.asset_category_id.id,
-                    'qty':self.quantity,
-                    'value': self.price_subtotal / self.quantity,
-                    'partner_id': self.move_id.partner_id.id,
-                    'company_id': self.move_id.company_id.id,
-                    'currency_id': self.move_id.company_currency_id.id,
-                    'date': self.move_id.invoice_date,
-                    'invoice_id': self.move_id.id,
-                }
-                print('-----vals---', vals)
-                changed_vals = self.env['account.asset.asset'].onchange_category_id_values(vals['category_id'])
-                vals.update(changed_vals['value'])
-                asset = self.env['account.asset.asset'].create(vals)
-                if self.asset_category_id.open_asset:
-                    asset.validate()
+            vals = {
+                'name': self.name,
+                'code': self.name or False,
+                'category_id': self.asset_category_id.id,
+                'value': self.price_subtotal,
+                'partner_id': self.move_id.partner_id.id,
+                'company_id': self.move_id.company_id.id,
+                'currency_id': self.move_id.company_currency_id.id,
+                'date': self.move_id.invoice_date,
+                'invoice_id': self.move_id.id,
+            }
+            changed_vals = self.env['account.asset.asset'].onchange_category_id_values(vals['category_id'])
+            vals.update(changed_vals['value'])
+            asset = self.env['account.asset.asset'].create(vals)
+            if self.asset_category_id.open_asset:
+                asset.validate()
         return True
 
     @api.onchange('asset_category_id')
