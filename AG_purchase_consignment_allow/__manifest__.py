546f7b7c04f8e25123d8c4e639ff2f909a5ef07d
bincy296 <71172217+bincy296@users.noreply.github.com>
2020-11-26 05:46:48 +0000

    Add files via upload
    modified(sana)

---
+++ AG_purchase_consignment_allow/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Part of Sananaz Mansuri See LICENSE file for full copyright and licensing details.
+{
+    'name': "AG Purchase Consignment Allow",
+    'version': '1.0.0',
+    'license': 'Other proprietary',
+    'category': 'Inventory',
+    'summary': """AG Purchase Consignment Allow""",
+    'description': """
+        AG Purchase Consignment Allow
+    """,
+    'author': "Sananaz Mansuri",
+    'website': 'www.odoo.com',
+    'live_test_url': '',
+    'depends': [
+        'AG_purchase_consignment',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
