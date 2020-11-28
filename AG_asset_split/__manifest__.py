ad75ccbdeb62e61912cf6c41bf536fdcd7944b12
bincy296 <71172217+bincy296@users.noreply.github.com>
2020-11-28 11:47:20 +0000

    Add files via upload

---
+++ AG_asset_split/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+{
+    'name': 'Split Assets In Vendor Bill',
+    'category': 'Purchase',
+    'summary': "Splitting of assets in vendor bill",
+    'depends': ['account','om_account_asset'],
+    'data': [
+
+        'views/asset_split.xml',
+        #'wizard/asset_dep_wizard.xml'
+    ],
+    'demo': [
+    ],
+    'license': 'AGPL-3',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
