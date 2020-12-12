cbbfda5438bd1d0d833cf26a3600801e01db00d9
Ziad-Monim <56960524+Ziad-Monim@users.noreply.github.com>
2020-12-12 13:21:25 +0400

    changes for unreconcile invoices and reversal approve in payment

--- AG_pdc_accounts/models/account_payment_inherit.py
+++ AG_pdc_accounts/models/account_payment_inherit.py
@@ -30,7 +30,7 @@ class account_payment(models.Model):
     effective_date = fields.Date('Effective Date',
                                  help='Effective date of PDC', copy=False,
                                  default=False)
-    state = fields.Selection([('draft', 'Draft'), ('posted', 'Validated'),('release', 'Released'),('reverse', 'Reversed'), ('sent', 'Sent'), ('reconciled', 'Reconciled'), ('cancelled', 'Cancelled')], readonly=True, default='draft', copy=False, string="Status")
+    state = fields.Selection([('draft', 'Draft'), ('posted', 'Validated'),('release', 'Released'),('approve', 'Reverse Approval'),('reverse', 'Reversed'), ('sent', 'Sent'), ('reconciled', 'Reconciled'), ('cancelled', 'Cancelled')], readonly=True, default='draft', copy=False, string="Status")
     currency_rate = fields.Float('Rate')
 
     @api.onchange('currency_id')
@@ -1175,6 +1175,59 @@ class account_payment(models.Model):
             'invoice_user_id': move.invoice_user_id.id,
         }
 
+    # def reverse_moves(self):
+    #     moves = self.env['account.move'].browse(self.env.context['active_ids']) if self.env.context.get('active_model') == 'account.move' else self.move_id
+
+    #     # Create default values.
+    #     default_values_list = []
+    #     for move in moves:
+    #         default_values_list.append(self._prepare_default_reversal(move))
+
+    #     batches = [
+    #         [self.env['account.move'], [], True],   # Moves to be cancelled by the reverses.
+    #         [self.env['account.move'], [], False],  # Others.
+    #     ]
+    #     for move, default_vals in zip(moves, default_values_list):
+    #         is_auto_post = bool(default_vals.get('auto_post'))
+    #         is_cancel_needed = not is_auto_post and self.refund_method in ('cancel', 'modify')
+    #         batch_index = 0 if is_cancel_needed else 1
+    #         batches[batch_index][0] |= move
+    #         batches[batch_index][1].append(default_vals)
+
+    #     # Handle reverse method.
+    #     moves_to_redirect = self.env['account.move']
+    #     for moves, default_values_list, is_cancel_needed in batches:
+    #         new_moves = moves._reverse_moves(default_values_list, cancel=is_cancel_needed)
+
+    #         if self.refund_method == 'modify':
+    #             moves_vals_list = []
+    #             for move in moves.with_context(include_business_fields=True):
+    #                 moves_vals_list.append(move.copy_data({'date': self.date or move.date})[0])
+    #             new_moves = self.env['account.move'].create(moves_vals_list)
+
+    #         moves_to_redirect |= new_moves
+
+    #     # Create action.
+    #     action = {
+    #         'name': _('Reverse Moves'),
+    #         'type': 'ir.actions.act_window',
+    #         'res_model': 'account.move',
+    #     }
+    #     if len(moves_to_redirect) == 1:
+    #         action.update({
+    #             'view_mode': 'form',
+    #             'res_id': moves_to_redirect.id,
+    #         })
+    #     else:
+    #         action.update({
+    #             'view_mode': 'tree,form',
+    #             'domain': [('id', 'in', moves_to_redirect.ids)],
+    #         })
+    #     return action
+
+    def reversal_approve(self):
+        self.write({'state': 'approve'})
+
     def reverse_moves(self):
         # moves = self.env['account.move'].browse(self.env.context['active_ids']) if self.env.context.get('active_model') == 'account.move' else self.move_id
         moves = self.mapped('move_line_ids.move_id')
@@ -1182,6 +1235,7 @@ class account_payment(models.Model):
         refund_method = (len(moves) > 1 or moves.type == 'entry') and 'cancel' or 'refund'
         default_values_list = []
         for move in moves:
+            move.button_cancel()
             default_values_list.append(self._prepare_default_reversal(move))
 
         batches = [
@@ -1198,6 +1252,7 @@ class account_payment(models.Model):
         # Handle reverse method.
         moves_to_redirect = self.env['account.move']
         for moves, default_values_list, is_cancel_needed in batches:
+            # raise UserError('test')
             new_moves = moves._reverse_moves(default_values_list, cancel=is_cancel_needed, PID=self.id)
 
             if refund_method == 'modify':
@@ -1209,7 +1264,11 @@ class account_payment(models.Model):
                     for rec in res.line_ids:
                         rec.write({'payment_id':self.id})
 
+            
+
             moves_to_redirect |= new_moves
+
+        
 
         self.write({'state': 'reverse'})
 
