627e00b8e5aee0977d00bcf693633d3c6defdcfe
Ziad-Monim <56960524+Ziad-Monim@users.noreply.github.com>
2020-12-15 11:38:17 +0400

    auto landed cost checkbox in PO

--- ag_po_landed_cost/models/purchase_order.py
+++ ag_po_landed_cost/models/purchase_order.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields,api
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
     is_landed_costs_line = fields.Boolean(
-        string='Landed Costs'
+        string='Landed Costs',compute='get_landed_cost',store=True
     )
+
+    @api.onchange('product_id')
+    def get_landed_cost(self):
+        for rec in self:
+            if rec.product_id:
+                rec.is_landed_costs_line = rec.product_id.landed_cost_ok
+            else:
+                rec.is_landed_costs_line = False
 
     def _prepare_account_move_line(self, move):
         res = super(PurchaseOrderLine, self)._prepare_account_move_line(move)
